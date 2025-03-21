0ece9fd471593bbe19ae6b82c6099571b19fc7c8
Gregory Nitch <nitch.gregory@outlook.com>
2025-03-22 07:06:35 +0900

    Renamed files, completed first llist impl, not tested yet

--- containers/llist/src/llist_59.c
+++ containers/llist/src/llist_59.c
@@ -15,6 +15,7 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
 ========================================================================================================================
@@ -23,7 +24,7 @@
 */
 
 #include "libc59_common.h"
-#include "llist.h"
+#include "llist_59.h"
 
 /*
 ========================================================================================================================
@@ -44,6 +45,8 @@
  **********************************************************************************************************************/
 ERR_59_e init_llist_59(llist_59 *llist, TYPE_59_e type, size_t type_depth)
 {
+    if (!llist)
+        return ERR_INV_PARAM;
 
     llist = malloc(sizeof(llist_59));
 
@@ -179,11 +182,10 @@ ERR_59_e pop_front_llist_59(llist_59 *llist, llist_node_59 *front_node)
 {
     if (!llist)
         return ERR_INV_PARAM;
+    if (!llist->head)
+        return ERR_CONTAINER_EMPTY;
 
     front_node = llist->head;
-
-    if (!front_node)
-        return ERR_CONTAINER_EMPTY;
 
     if (llist->head->next)
         llist->head = llist->head->next;
@@ -191,6 +193,7 @@ ERR_59_e pop_front_llist_59(llist_59 *llist, llist_node_59 *front_node)
         llist->head = (void *)0;
 
     front_node->next = (void *)0;
+
     return ERR_NONE;
 }
 
@@ -209,9 +212,10 @@ ERR_59_e remove_given_node_from_llist_59(llist_59 *llist, llist_node_59 *remove_
         return ERR_INV_PARAM;
 
     llist_node_59 *node = llist->head;
-    llist_node_59 *last_node = (void *)0;
     if (!node)
         return ERR_CONTAINER_EMPTY;
+
+    llist_node_59 *last_node = (void *)0;
     while (node)
     {
         if (node == remove_node)
@@ -232,27 +236,187 @@ ERR_59_e remove_given_node_from_llist_59(llist_59 *llist, llist_node_59 *remove_
     return ERR_OBJ_NOT_FOUND;
 }
 
+/***********************************************************************************************************************
+ * @brief : Inserts a node into the linked list at the passed index, if the index is passed the end of the list it
+ * appends the node to the end of the list.
+ *
+ * @param[in] llist : Linked list to add the new node too.
+ * @param[in] new_node : New node to add to the linked list.
+ * @param[in] idx : Index in the linked list to add the node at.
+ *
+ * @retval ERR_59_e : error value encountered during the function call, ERR_NONE = all ok.
+ **********************************************************************************************************************/
 ERR_59_e insert_node_into_llist_59(llist_59 *llist, llist_node_59 *new_node, size_t idx)
 {
-    // TODO: . . .
+    if (!llist || !new_node)
+        return ERR_INV_PARAM;
+
+    if (!llist->head)
+    {
+        llist->head = new_node;
+        return ERR_NONE;
+    }
+
+    llist_node_59 *node = llist->head;
+    llist_node_59 *last_node = (void *)0;
+    for (size_t i = 0; i <= idx; i++)
+    {
+        if (!node)
+            break;
+
+        last_node = node;
+        node = node->next;
+    }
+    last_node->next = new_node;
+    new_node->next = node;
+
+    return ERR_NONE;
 }
 
+/***********************************************************************************************************************
+ * @brief : Initializes a node for a @llist_59, @next and @node_obj can be NULL.
+ *
+ * @param[in] node : Pointer to initialize new node in.
+ * @param[in] next : Next node in the llinked list to point at, may be NULL.
+ * @param[in] node_obj : Object that the new node shall point to, may be NULL.
+ *
+ * @retval ERR_59_e : error value encountered during the function call, ERR_NONE = all ok.
+ **********************************************************************************************************************/
 ERR_59_e init_llist_node_59(llist_node_59 *node, llist_node_59 *next, void *node_obj)
 {
-    // TODO: . . .
+    if (!node)
+        return ERR_INV_PARAM;
+
+    node = malloc(sizeof(llist_node_59));
+
+    if (!node)
+        return ERR_NO_MEM;
+
+    node->next = next;
+    node->node_obj = node_obj;
+
+    return ERR_NONE;
 }
 
+/***********************************************************************************************************************
+ * @brief : Destroys a linked list node (deallocates memory for both the node and the void pointer object)
+ *
+ * @param[in] node : Node to destroy.
+ *
+ * @retval ERR_59_e : error value encountered during the function call, ERR_NONE = all ok.
+ **********************************************************************************************************************/
 ERR_59_e destroy_llist_node_59(llist_node_59 *node)
 {
-    // TODO: . . .
+    if (!node)
+        return ERR_INV_PARAM;
+
+    if (node->node_obj)
+        free(node->node_obj);
+
+    node->node_obj = (void *)0;
+    node->next = (void *)0;
+    free(node);
+
+    return ERR_NONE;
 }
+
+/***********************************************************************************************************************
+ * @brief : Compares two list nodes such that if the value of @node_A is greater than @node_B a positive value is
+ * returned in @diff_out, or a negative when @node_B is greater than @node_A, or 0 when they are equal.
+ *
+ * @param[in] llist : Linked List that the nodes belong to, the type struct member will be used to determine the
+ * comparison path.
+ * @param[in] node_A : Node to compare.
+ * @param[in] node_B : Other node to compare.
+ * @param[out] diff_out : Difference determined based on the difference between the node_obj members of the nodes.
+ *
+ * @note Strings when compared will produce negative values for strings that come first alphabetically. ie, a - b = -1
+ * @note Only only signed, unsigned, chars, and strings are supported by this function. ERR_NOT_SUPPORTED will be
+ * returned.
+ *
+ * @retval ERR_59_e : error value encountered during the function call, ERR_NONE = all ok.
+ **********************************************************************************************************************/
 
 ERR_59_e compare_llist_nodes_59(llist_59 *llist, llist_node_59 *node_A, llist_node_59 *node_B, i64 *diff_out)
 {
-    // TODO: . . .
+    if (!llist || !node_A || !node_B || !diff_out)
+        return ERR_INV_PARAM;
+
+    switch (llist->type)
+    { // TODO : properly test return of unsigned types
+    // Unsigned
+    case U8_PTR:
+        *diff_out = *((u8 *)node_A->node_obj) - *((u8 *)node_B->node_obj);
+        break;
+
+    case U16_PTR:
+        *diff_out = *((u16 *)node_A->node_obj) - *((u16 *)node_B->node_obj);
+        break;
+
+    case U32_PTR:
+        *diff_out = *((u32 *)node_A->node_obj) - *((u32 *)node_B->node_obj);
+        break;
+
+    case U64_PTR:
+        *diff_out = *((u64 *)node_A->node_obj) - *((u64 *)node_B->node_obj);
+        break;
+
+    case SIZE_PTR:
+        *diff_out = *((size_t *)node_A->node_obj) = *((size_t *)node_B->node_obj);
+        break;
+
+    // Signed
+    case I8_PTR:
+        *diff_out = *((i8 *)node_A->node_obj) - *((i8 *)node_B->node_obj);
+        break;
+
+    case I16_PTR:
+        *diff_out = *((i16 *)node_A->node_obj) - *((i16 *)node_B->node_obj);
+        break;
+
+    case I32_PTR:
+        *diff_out = *((i32 *)node_A->node_obj) - *((i32 *)node_B->node_obj);
+        break;
+
+    case I64_PTR:
+        *diff_out = *((i64 *)node_A->node_obj) - *((i64 *)node_B->node_obj);
+        break;
+
+    // Other
+    case CHAR_PTR: // NOTE : This is not a string (only a single char)
+        *diff_out = *((char *)node_A->node_obj) - *((char *)node_B->node_obj);
+        break;
+
+    case STR: // NOTE : This is an array of chars terminated by '\0', ie a string
+        *diff_out = strcmp((char *)node_A->node_obj, (char *)node_B->node_obj);
+        break;
+
+    default: // We are trying to compare something that this function has not been made for. Such as a struct
+        return ERR_NOT_SUPPORTED;
+    }
+
+    return ERR_NONE;
 }
 
+/***********************************************************************************************************************
+ * @brief : Compares two linked list node pointers, if they point to the same address they are the same node. Returns
+ * false when one or both nodes are NULL.
+ *
+ * @param[in] node_A : Node to compare.
+ * @param[in] node_B : Other node to compare.
+ * @param[out] is_same : bool value to return for boolean logic.
+ *
+ * @retval ERR_59_e : error value encountered during the function call, ERR_NONE = all ok.
+ **********************************************************************************************************************/
 ERR_59_e is_same_llist_node_59(llist_node_59 *node_A, llist_node_59 *node_B, bool *is_same)
 {
-    // TODO: . . .
+    if (!is_same)
+        return ERR_INV_PARAM;
+
+    if (!node_A || !node_B)
+        *is_same = false;
+    else
+        *is_same = node_A == node_B;
+
+    return ERR_NONE;
 }

a48a97c0024dfb710f8e0a040b8bd4ce77cdae36
Gregory Nitch <nitch.gregory@outlook.com>
2025-03-27 06:23:45 +0900

    added begining of llist unit test, updated cmake for test dir output and lib linking, test failing with segfault

--- containers/llist/test/src/test_llist_59.c
+++ containers/llist/test/src/test_llist_59.c
@@ -31,6 +31,41 @@
 ========================================================================================================================
 */
 
+ERR_59_e test_list_manipulations(void)
+{
+    ERR_59_e err = ERR_NONE;
+
+    // Init list and nodes
+    llist_59 *list;
+    err = init_llist_59(list, U64, 0);
+    if (err != ERR_NONE)
+        return err;
+
+    for (size_t i = 1; i < 6; i++)
+    {
+        llist_node_59 *node;
+        err = init_llist_node_59(node, (void *)0, malloc(sizeof(u64)));
+        if (err != ERR_NONE)
+            return err;
+
+        *((u64 *)node->node_obj) = (u64)i;
+
+        err = push_back_llist_59(list, node);
+        if (err != ERR_NONE)
+            return err;
+    }
+
+    // Verify list contents
+    llist_node_59 *node = list->head;
+    for (size_t i = 1; i < 6; i++)
+    {
+        assert(*((u64 *)node->node_obj) == i);
+        node = node->next;
+    }
+
+    return err;
+}
+
 /*
 ========================================================================================================================
 - - MAIN - -
@@ -40,6 +75,9 @@
 int main(int argc, char const *argv[])
 {
     puts("- - -  START OF LLIST TEST  - - -");
+
+    assert(test_list_manipulations() == ERR_NONE);
+
     puts("- - - - END OF LLIST TEST - - - -");
     return EXIT_SUCCESS;
 }

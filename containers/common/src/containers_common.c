0df1f755794d5086dd6477f11b89850271393f53
Gregory Nitch <nitch.gregory@gmail.com>
2026-06-07 12:49:40 +0900

    updated node compare verbiage for better understanding

--- containers/common/src/containers_common.c
+++ containers/common/src/containers_common.c
@@ -60,7 +60,7 @@
  * @param[in] obj_B: Other obj to compare.
  * @param[out] diff_out: Difference determined based on the difference between the objs.
  *
- * @note Strings when compared will produce negative values for strings that come first alphabetically. ie, a - b = -1
+ * @note Strings when compared will produce negative values for strings that come first alphabetically. ie, a - b = -1 where a = "abc" and b = "cba".
  * @note Only signed, unsigned, chars, and strings are supported by this function. ERR_NOT_SUPPORTED will be
  * returned.
  * @note u64 and size_t values have a limit on @diff_out values due to memory restraints of the i64 return value. If the
